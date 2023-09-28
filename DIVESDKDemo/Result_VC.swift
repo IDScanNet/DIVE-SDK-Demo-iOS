70eb309552aefb34a0a4740a4a96f6746b828cee
Alexander Korotkov <akorotkov@abyka.com>
2023-09-28 18:17:10 +0500

    Initial

---
+++ DIVESDKDemo/Result_VC.swift
@@ -0,0 +1,17 @@
+//
+//  Result_VC.swift
+//  DIVESDKDemo
+//
+//  Created by AKorotkov on 19.12.2022.
+//
+
+import UIKit
+
+class Result_VC: UIViewController {
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# [DIVE SDK](https://github.com/IDScanNet/DIVE-SDK-iOS) Demo for iOS
+
+This sample application is meant to demostrate how to use the iOS SDK with the DIVE API
